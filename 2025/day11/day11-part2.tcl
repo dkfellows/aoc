2df06fb2bf1d428fcc1a8c8a88c037816cc6547d
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-12-12 15:27:20 +0000

    Use TclOO to get slightly nicer scoping

--- 2025/day11/day11-part2.tcl
+++ 2025/day11/day11-part2.tcl
@@ -6,101 +6,81 @@ proc readfile {name} {
 	try {read $f} finally {close $f}
 }
 
-proc parse-data {data} {
+oo::class create PathFollower {
+	variable s src tgt cache
+
+	# Find what states are reachable in a particular direction
+	method Reachable {*table states start} {
+		upvar 1 ${*table} table
+		array default set table 0
+		set table($start) 1
+		set layer [list $start]
+		while {[llength $layer]} {
+			set nextlayer {}
+			foreach item $layer {
+				lappend nextlayer {*}[dict getdef $states $item {}]
+			}
+			set layer [lsort -unique $nextlayer]
+			foreach next $layer {set table($next) 1}
+		}
+	}
+
+	# A node is relevant if it is both downstream of a source and upstream of a target
+	constructor {states source target} {
+		set src $source
+		set tgt $target
+		array set cache {}; # Memoisation cache
+
+		my Reachable downstream $states $source
+		# invert the flow
+		dict for {from to} $states {foreach to $to {dict lappend backward $to $from}}
+		my Reachable upstream $backward $target
+
+		# Remove all entries not both downstream and upstream marked to make working subgraph
+		set s [dict map {from to} $states {
+			if {$downstream($from) && $upstream($from)} {set to} continue
+		}]
+	}
+
+	# Classic graph follower for a NON-LOOPING graph
+	method Count-paths {me} {
+		if {[info exists cache($me)]} {
+			return $cache($me)
+		}
+		set cache($me) [tcl::mathop::+ {*}[lmap next [dict getdef $s $me {}] {
+			expr {$next eq $tgt ? 1 : [my Count-paths $next]}
+		}]]
+	}
+	method count-paths {} {
+		tailcall my Count-paths $src
+	}
+
+	self method gc {} {
+		foreach o [info class instances [self]] {$o destroy}
+	}
+}
+
+proc problem {data} {
 	foreach line [split [string trim $data] \n] {
 		lassign [split $line :] source destinations
 		dict set states $source [split [string trim $destinations]]
 	}
-	return $states
+
+	# Path could go in either order; compute both
+
+	# Path: svr ---> fft ---> dac ---> out
+	set svrfft [[PathFollower new $states svr fft] count-paths]
+	set fftdac [[PathFollower new $states fft dac] count-paths]
+	set dacout [[PathFollower new $states dac out] count-paths]
+
+	# Path: svr ---> dac ---> fft ---> out
+	set svrdac [[PathFollower new $states svr dac] count-paths]
+	set dacfft [[PathFollower new $states dac fft] count-paths]
+	set fftout [[PathFollower new $states fft out] count-paths]
+
+	PathFollower gc
+
+	return [expr {$dacout * $fftdac * $svrfft + $fftout * $dacfft * $svrdac}]
 }
 
-# A node is relevant if it is both downstream of a source and upstream of a target
-proc relevant-subset {states source target} {
-	array default set downstream 0
-	array default set upstream 0
-
-	set downstream($source) 1
-	set this $source
-	while {[llength $this]} {
-		set nextlayer {}
-		foreach item $this {
-			set seen [dict getdef $states $item {}]
-			lappend nextlayer {*}$seen
-		}
-		set nextlayer [lsort -unique $nextlayer]
-		foreach next $nextlayer {
-			set downstream($next) 1
-		}
-		set this $nextlayer
-	}
-
-	# invert the flow
-	dict for {from to} $states {foreach to $to {dict lappend backward $to $from}}
-
-	set upstream($target) 1
-	set this $target
-	while {[llength $this]} {
-		set nextlayer {}
-		foreach item $this {
-			set seen [dict getdef $backward $item {}]
-			lappend nextlayer {*}$seen
-		}
-		set nextlayer [lsort -unique $nextlayer]
-		foreach next $nextlayer {
-			set upstream($next) 1
-		}
-		set this $nextlayer
-	}
-
-	# Remove all entries not both downstream and upstream marked
-	dict map {from to} $states {
-		if {!$downstream($from) || !$upstream($from)} continue
-		set to
-	}
-}
-
-# Classic graph follower for a NON-LOOPING graph
-proc follow-paths {states path target} {
-	global cache; # Memoisation cache; assume one fundamental graph per run
-	set count 0
-	set me [lindex $path end]
-	if {[info exists cache($me,$target)]} {
-		return $cache($me,$target)
-	}
-	foreach next [dict getdef $states $me {}] {
-		set path1 $path
-		lappend path1 $next
-		if {$next eq $target} {
-			incr count
-		} else {
-			incr count [follow-paths $states $path1 $target]
-		}
-	}
-	return [set cache($me,$target) $count]
-}
-
-proc problem {states {print 0}} {
-	set rel [relevant-subset $states dac out]
-	set dacout [follow-paths $rel dac out]
-	if {$print} {puts "dac-out: [dict size $rel] -> $dacout"}
-	set rel [relevant-subset $states fft dac]
-	set fftdac [follow-paths $rel fft dac]
-	if {$print} {puts "fft-dac: [dict size $rel] -> $fftdac"}
-	set rel [relevant-subset $states svr fft]
-	set svrfft [follow-paths $rel svr fft]
-	if {$print} {puts "svr-fft: [dict size $rel] -> $svrfft"}
-
-	set rel [relevant-subset $states fft out]
-	set fftout [follow-paths $rel fft out]
-	if {$print} {puts "fft-out: [dict size $rel] -> $fftout"}
-	set rel [relevant-subset $states dac fft]
-	set dacfft [follow-paths $rel dac fft]
-	if {$print} {puts "dac-fft: [dict size $rel] -> $dacfft"}
-	set rel [relevant-subset $states svr dac]
-	set svrdac [follow-paths $rel svr dac]
-	if {$print} {puts "svr-dac: [dict size $rel] -> $svrdac"}
-
-	expr {$dacout * $fftdac * $svrfft + $fftout * $dacfft * $svrdac}
-}
-
-puts [problem [parse-data [readfile [lindex $argv 0]]] {*}[lrange $argv 1 end]]
+puts [problem [readfile [lindex $argv 0]]]
