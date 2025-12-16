f2be5fbf0cf5682ee01aa6045b69ddf731312442
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-12-16 09:51:43 +0000

    Track indices; "better for future use"
    As if I'm ever going to do that...

--- 2025/day12/day12-part1.tcl
+++ 2025/day12/day12-part1.tcl
@@ -27,6 +27,7 @@ proc parse-data {data} {
 # A basic check for sanity (claus)
 proc check-basic-area {data} {
 	lassign $data presents config
+	set cfgi [lseq {[llength $config] / 3}]
 
 	# Discover which configurations are trivially easy
 	foreach present [dict values $presents] {
@@ -37,26 +38,26 @@ proc check-basic-area {data} {
 	set px [lindex $pxs 0]
 	if {[llength [set pys [lsort -unique -integer $pys]]] != 1} {error "non-unique Y size: $pys"}
 	set py [lindex $pys 0]
-	set definitelySucceeded [lmap {x y selector} $config {
+	set definitelySucceeded [lmap {x y selector} $config i $cfgi {
 		if {[llength $selector] > ($x / $px) * ($y / $py)} continue
-		string cat $x , $y
+		set i
 	}]
 
 	# Discover which configurations are trivially impossible
 	set sizes [dict map {idx pdata} $presents {regexp -all # $pdata}]
-	set definitelyFailed [lmap {x y selector} $config {
+	set definitelyFailed [lmap {x y selector} $config i $cfgi {
 		if {
 			[tcl::mathop::+ {*}[lmap idx $selector {
 				dict get $sizes $idx
 			}]] <= $x*$y
 		} continue
-		string cat $x , $y
+		set i
 	}]
 
 	# Tricky cases are ones that are neither trivial or impossible
-	foreach {x y -} $config {dict set tricky $x,$y ok}
-	foreach xy $definitelySucceeded {dict unset tricky $xy}
-	foreach xy $definitelyFailed {dict unset tricky $xy}
+	foreach i $cfgi {dict set tricky $i ok}
+	foreach i $definitelySucceeded {dict unset tricky $i}
+	foreach i $definitelyFailed {dict unset tricky $i}
 	set tricky [dict keys $tricky]
 
 	return easy:[llength $definitelySucceeded],impossible:[llength $definitelyFailed],tricky:[llength $tricky]
