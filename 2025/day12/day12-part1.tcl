501199ac34c84d08137aa7943feb7511690cf4db
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-12-16 09:45:40 +0000

    Better solution for Day 12

--- 2025/day12/day12-part1.tcl
+++ 2025/day12/day12-part1.tcl
@@ -27,16 +27,39 @@ proc parse-data {data} {
 # A basic check for sanity (claus)
 proc check-basic-area {data} {
 	lassign $data presents config
+
+	# Discover which configurations are trivially easy
+	foreach present [dict values $presents] {
+		lappend pxs [string length [lindex $present 0]]
+		lappend pys [llength $present]
+	}
+	if {[llength [set pxs [lsort -unique -integer $pxs]]] != 1} {error "non-unique X size: $pxs"}
+	set px [lindex $pxs 0]
+	if {[llength [set pys [lsort -unique -integer $pys]]] != 1} {error "non-unique Y size: $pys"}
+	set py [lindex $pys 0]
+	set definitelySucceeded [lmap {x y selector} $config {
+		if {[llength $selector] > ($x / $px) * ($y / $py)} continue
+		string cat $x , $y
+	}]
+
+	# Discover which configurations are trivially impossible
 	set sizes [dict map {idx pdata} $presents {regexp -all # $pdata}]
-	set basicPass 0
-	foreach {x y selector} $config {
-		incr basicPass [expr {
+	set definitelyFailed [lmap {x y selector} $config {
+		if {
 			[tcl::mathop::+ {*}[lmap idx $selector {
 				dict get $sizes $idx
 			}]] <= $x*$y
-		}]
-	}
-	return $basicPass
+		} continue
+		string cat $x , $y
+	}]
+
+	# Tricky cases are ones that are neither trivial or impossible
+	foreach {x y -} $config {dict set tricky $x,$y ok}
+	foreach xy $definitelySucceeded {dict unset tricky $xy}
+	foreach xy $definitelyFailed {dict unset tricky $xy}
+	set tricky [dict keys $tricky]
+
+	return easy:[llength $definitelySucceeded],impossible:[llength $definitelyFailed],tricky:[llength $tricky]
 }
 
 puts [check-basic-area [parse-data [readfile [lindex $argv 0]]]]
